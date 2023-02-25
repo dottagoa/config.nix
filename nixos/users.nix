0ac715447a6ec801acbb4440eceef58f53212793
Zyxophobia <hello@zyxophobia.cc>
2023-02-24 21:51:15 -0500

    Add outputs delimiter to nixos/users.nix

--- nixos/users.nix
+++ nixos/users.nix
@@ -1,10 +1,10 @@
-{ inputs, ...}: {
+{ inputs, outputs, ...}: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
   ];
 
   home-manager = {
-    extraSpecialArgs = { inherit inputs; };
+    extraSpecialArgs = { inherit inputs outputs; };
     users = {
       zyx = import ../home-manager/home.nix;
     };
